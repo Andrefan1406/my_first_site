bee951f37ba6389d85ee86682f07b921c1a9bfe0
Кузнецов Андрей Евгеньевич <a.kuznecov@vkdev.kz>
2025-07-21 13:32:50 +0500

    добавил изделия

--- src/BLBRequestPage.js
+++ src/BLBRequestPage.js
@@ -7,6 +7,13 @@ import {
  } from './data/constructionData2';
 import Select from 'react-select';
 
+const productBrandOptions = {
+    'Брусчатка' : ["Б.1.П.7",  "Б.2.П.7", "Б.3.П.7", "Б.5.П.7", "Б.6.П.7", "Б.7.П.7", "Б.8.П.7", 
+        "Б.9.П.7", "Б.10.П.7", "Б.11.П.7", "Б.12.П.7", "Б.13.П.7", "Б.14.П.7", "Б.15.П.7"],
+    'Бордюр' : ["БР 100.30.15"],
+    'Лоток' : ["50.25.9"]
+};
+
 const BLBRequestPage = () => {
     const [formRows, setFormRows] = useState([
       {
